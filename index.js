c4bf679050119084ea13dd41d1e4da582f5a0bcb
javier06github <145624377+javier06github@users.noreply.github.com>
2025-12-15 21:29:22 -0300

    Inicial

--- index.js
+++ index.js
@@ -1,33 +1,18 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
-import rutasAuth from "./src/routes/auth.routes.js";
-import rutasProducts from "./src/routes/products.routes.js";
-
 dotenv.config();
 
+import productsRouter from "./src/routes/products.routes.js";
+import authRouter from "./src/routes/auth.routes.js";
+
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-// Middlewares globales
-app.use(cors({ origin: process.env.CORS_ORIGIN || "*" }));
-app.use(express.json()); // parser JSON
+app.use("/api/products", productsRouter);
+app.use("/auth", authRouter);
 
-// Rutas
-app.use("/auth", rutasAuth);
-app.use("/api/products", rutasProducts);
+app.get("/", (req, res) => res.send("API OK"));
 
-// 404 rutas no definidas
-app.use((req, res) => {
-  res.status(404).json({ error: "Ruta no encontrada" });
-});
-
-// Handler general de errores (400/500)
-app.use((err, req, res, next) => {
-  const status = err.status || 500;
-  const msg = err.message || "Error interno del servidor";
-  res.status(status).json({ error: msg });
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor escuchando en puerto ${PORT}`));
+export default app;
